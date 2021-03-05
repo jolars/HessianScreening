fca873e860dbdde99efb5ef07dcadafb958f703f
Johan Larsson <johanlarsson@outlook.com>
2021-03-05 17:00:42 +0100

    Remove Julia stuff from repository

--- julia/HessianScreeningCode.jl
+++
@@ -1,4 +0,0 @@
-include("lassoPath.jl")
-include("generateDesign.jl")
-include("readLIBSVM.jl")
-include("getData.jl")
--- julia/prox.jl
+++
@@ -1,4 +0,0 @@
-# soft-thresholding (lasso prox)
-function prox(x, λ)
-    sign(x) * max(abs(x) - λ, 0.0)
-end
