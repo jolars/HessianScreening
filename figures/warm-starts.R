0bf2201c6344abb3f16af085a7737fb97af0f1ef
Johan Larsson <johanlarsson@outlook.com>
2021-04-14 19:36:14 +0200

    Change colors in viz

--- figures/warm-starts.R
+++ figures/warm-starts.R
@@ -7,9 +7,9 @@ theme_set(theme_minimal(base_size = 9))
 dat <- readRDS("results/warm-starts.rds")
 
 tikz("figures/hessian-warm-starts.tex", width = 4.7, height = 2)
-ggplot(dat, aes(Step, Passes, col = WarmStart, linetype = WarmStart)) +
+ggplot(dat, aes(Step, Passes, col = WarmStart)) +
   geom_step() +
   facet_wrap(~dataset, scales = "free") +
   labs(col = "Warm Start", linetype = "Warm Start") +
-  scale_color_manual(values = c("dark grey", "dark orange"))
+  scale_color_manual(values = c("dark orange", "black"))
 dev.off()
