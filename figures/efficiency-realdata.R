262214e9d2af9b0942f4fb86e792a974f75b7153
Johan Larsson <johanlarsson@outlook.com>
2021-04-19 19:30:49 +0200

    Add figure script for efficiency

---
+++ figures/efficiency-realdata.R
@@ -0,0 +1,55 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(lemon)
+library(tikzDevice)
+
+theme_set(theme_minimal(base_size = 9))
+
+d <- readRDS("results/efficiency-realdata.rds")
+
+# https://stackoverflow.com/questions/54438495/shift-legend-into-empty-facets-of-a-faceted-plot-in-ggplot2
+shift_legend2 <- function(p) {
+  # ...
+  # to grob
+  gp <- ggplotGrob(p)
+  facet.panels <- grep("^panel", gp[["layout"]][["name"]])
+  empty.facet.panels <- sapply(
+    facet.panels, function(i) "zeroGrob" %in% class(gp[["grobs"]][[i]]))
+  empty.facet.panels <- facet.panels[empty.facet.panels]
+
+  # establish name of empty panels
+  empty.facet.panels <- gp[["layout"]][empty.facet.panels, ]
+  names <- empty.facet.panels$name
+  # example of names:
+  #[1] "panel-3-2" "panel-3-3"
+
+# now we just need a simple call to reposition the legend
+  reposition_legend(p, 'center', panel=names)
+}
+
+d2 <-
+  d %>%
+  mutate(screening_type = recode(screening_type,
+         "edpp" = "EDPP",
+         "gap_safe" = "Gap Safe",
+         "hessian" = "Hessian",
+         "strong" = "Strong")) %>%
+  drop_na() %>%
+  unnest(c(screened, active)) %>%
+  group_by(dataset, screening_type) %>%
+  mutate(screened = screened/p, active = active/p) %>%
+  mutate(step = seq_along(screened))
+
+pl <- ggplot(d2, aes(step)) +
+  facet_wrap(~dataset, nrow = 2) +
+  geom_line(aes(y = active), linetype = 2) +
+  geom_line(aes(y = screened, col = screening_type)) +
+  scale_y_continuous(oob = function(x, ...) {x},
+                     limits = c(0, 0.06)) +
+  theme(legend.title = element_blank()) +
+  scale_color_manual(values = cols)
+
+tikz("figures/realdata-efficiency.tex", width = 5.6, height = 2.5)
+shift_legend2(pl)
+dev.off()
