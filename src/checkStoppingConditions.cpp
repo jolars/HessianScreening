02bab7764cd838f379e1a075446bafaa31458fc4
Johan Larsson <johanlarsson@outlook.com>
2021-11-23 15:05:32 +0100

    Split .h into .h and .cpp

---
+++ src/checkStoppingConditions.cpp
@@ -0,0 +1,56 @@
+#include "checkStoppingConditions.h"
+#include <RcppArmadillo.h>
+
+bool
+checkStoppingConditions(const arma::uword step,
+                        const arma::uword n,
+                        const arma::uword p,
+                        const arma::uword n_lambda,
+                        const arma::uword n_active,
+                        const double lambda,
+                        const double lambda_min,
+                        const double dev,
+                        const double dev_prev,
+                        const double null_dev,
+                        const std::string screening_type,
+                        const arma::uword verbosity)
+{
+  if (step == 1) {
+    // first step; always continue
+    return false;
+  }
+
+  if (verbosity >= 1) {
+    Rprintf("  checking stopping conditions\n");
+  }
+
+  double dev_ratio = 1.0 - dev / null_dev;
+  double dev_change = 1.0 - dev / dev_prev;
+
+  if (verbosity >= 1) {
+    Rprintf(
+      "    dev ratio:  %.3f\n    dev change: %.6f\n", dev_ratio, dev_change);
+  }
+
+  if (dev_change <= 1e-5 && screening_type != "hessian_adaptive") {
+    return true;
+  }
+
+  if (dev_ratio >= 0.999) {
+    return true;
+  }
+
+  if (screening_type != "hessian_adaptive" && lambda <= lambda_min) {
+    return true;
+  }
+
+  if (n <= p && n_active >= n) {
+    return true;
+  }
+
+  if (step >= n_lambda) {
+    return true;
+  }
+
+  return false;
+}
