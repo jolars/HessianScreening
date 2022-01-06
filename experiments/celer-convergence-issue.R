7a8603dfa2e2893ad327915deea8d2f8ede8f18d
Johan Larsson <johanlarsson@outlook.com>
2022-01-06 09:45:55 +0100

    Upload example of failure to converge

---
+++ experiments/celer-convergence-issue.R
@@ -0,0 +1,43 @@
+library(HessianScreening)
+
+set.seed(3)
+
+family <- "binomial"
+
+set.seed(14)
+d <- generateDesign(1000, 100, family = family)
+X <- d$X
+y <- d$y
+
+if (family != "binomial") {
+  y <- y - mean(y)
+}
+
+n <- nrow(X)
+p <- ncol(X)
+verbosity <- 2
+line_search <- 3
+tol_gap <- 1e-4
+maxit <- 1e7
+standardize <- FALSE
+
+fit_celer <- lassoPath(
+  X,
+  y,
+  family = family,
+  screening_type = "celer",
+  standardize = standardize,
+  verbosity = verbosity,
+  tol_gap = tol_gap,
+  line_search = line_search,
+  gap_safe_active_start = TRUE,
+  celer_use_accel = FALSE,
+  celer_prune = TRUE,
+  maxit = maxit
+)
+
+real_gaps <- duality_gaps(fit_celer, family, standardize, X, y)$gaps
+
+celer_gaps <- (fit_celer$primals - fit_celer$duals) / fit_celer$primals
+
+print(cbind(real_gaps, celer_gaps))
