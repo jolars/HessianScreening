cac7bf3341c566d66e8769dd8ccdebc5922a355c
Johan Larsson <johanlarsson@outlook.com>
2022-01-18 10:06:05 +0100

    Add new experiment

---
+++ experiments/ws-screening-contrib.R
@@ -0,0 +1,51 @@
+library(HessianScreening)
+library(tibble)
+library(dplyr)
+library(tidyr)
+
+printf <- function(...) invisible(cat(sprintf(...)))
+
+d <- readRDS("data/YearPredictionMSD-train.rds")
+X <- d$X
+y <- d$y
+
+fit_warm1 <- lassoPath(X, y,
+  hessian_warm_starts = TRUE,
+  screening_type = "hessian"
+)
+fit_std1 <- lassoPath(X, y,
+  hessian_warm_starts = FALSE,
+  screening_type = "hessian"
+)
+
+d <- readRDS("data/colon-cancer.rds")
+X <- d$X
+y <- d$y
+
+fit_warm2 <- lassoPath(X, y,
+  family = "binomial",
+  hessian_warm_starts = TRUE,
+  screening_type = "hessian"
+)
+fit_std2 <- lassoPath(X, y,
+  family = "binomial",
+  hessian_warm_starts = FALSE,
+  screening_type = "hessian"
+)
+
+n1 <- length(fit_warm1$lambda)
+n2 <- length(fit_warm2$lambda)
+
+dat <- tibble(
+  dataset = rep(c("YearPredictionMSD", "colon-cancer"), times = c(n1, n2)),
+  Step = c(1:n1, 1:n2),
+  Hessian = c(fit_warm1$passes, fit_warm2$passes),
+  Standard = c(fit_std1$passes, fit_std2$passes)
+) %>%
+  pivot_longer(c("Hessian", "Standard"),
+    names_to = "WarmStart",
+    values_to = "Passes"
+  ) %>%
+  mutate(Passes = as.integer(Passes))
+
+saveRDS(dat, "results/warm-starts.rds")
