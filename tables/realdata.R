17fad72e0a46182f19adf370780be68178ff8216
Johan Larsson <johanlarsson@outlook.com>
2022-05-19 12:23:47 +0200

    chore: update results table script

--- tables/realdata.R
+++ tables/realdata.R
@@ -25,6 +25,10 @@ d <-
       "celer" = "Celer",
       "blitz" = "Blitz"
     ),
+    dataset = recode(
+      dataset,
+      "bc_tcga" = "bcTCGA"
+    ),
     model = recode(
       family,
       "gaussian" = "Least-Squares",
